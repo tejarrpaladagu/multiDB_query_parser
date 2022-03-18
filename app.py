db2a2c7b4b6bbbd6518f8a13be964a95fdda61eb
Vamshi900 <vamshigeek1@gmail.com>
2022-03-18 01:17:20 -0400

    added timer, execption handling ,aggergate queries

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import pymysql
 import json
 import config
 from flask_cors import CORS
+import time
+# import psycopg2
 
 app = Flask(__name__)
 
@@ -15,15 +17,31 @@ CORS function is to avoid No 'Access-Control-Allow-Origin' error
 """
 CORS(app)
 
+# set default function for jsonify
+def set_default(obj):
+    if isinstance(obj, set):
+        return list(obj)
+    raise TypeError
+
+
+# lamda takes data returns json response
+def response(data): return app.response_class(
+    response=json.dumps(data, default=set_default),
+    status=200,
+    mimetype='application/json'
+)
+
+# type handler for jsonify 
 def type_handler(x):
     if isinstance(x, datetime.datetime):
         return x.isoformat()
     if isinstance(x, decimal.Decimal):
-        return '$%.2f'%(x)
+        return '$%.2f' % (x)
     raise TypeError("Unknown type")
 
-def rows_to_json(cols,rows):
-
+# jsonify function with custom type handler
+# convert datetime to isoformat and decimal to string
+def rows_to_json(cols, rows):
     result = []
     for row in rows:
         data = dict(zip(cols, row))
@@ -33,72 +51,137 @@ def rows_to_json(cols,rows):
 
 @app.route('/')
 def hello():
-    # test api 
+    # test api
     return 'Welcome Api setup sucessful'
+
 
 @app.route('/test')
 def test_get():
- 
-    # create mysql connection
-    conn = pymysql.connect(host=config._DB_CONF['host'], 
-                           port=config._DB_CONF['port'], 
-                           user=config._DB_CONF['user'], 
-                           passwd=config._DB_CONF['passwd'], 
-                           db=config._DB_CONF['db'])
-    cur = conn.cursor()
-    sql="SELECT * FROM mytable LIMIT 3;"
-    cur.execute(sql)
-    
-    # get all column names
-    columns = [desc[0] for desc in cur.description]
-    # get all data
-    rows=cur.fetchall()
-    
-    # build json 
-    result = rows_to_json(columns,rows)
-    #print(result)
-    
-    cur.close()
-    conn.close()
+    try:
+        # create mysql connection
+        conn = pymysql.connect(host=config._DB_CONF['host'],
+                               port=config._DB_CONF['port'],
+                               user=config._DB_CONF['user'],
+                               passwd=config._DB_CONF['passwd'],
+                               db=config._DB_CONF['db'])
+        cur = conn.cursor()
+        # caluclating the time from now
+        start_time = time.time()
 
-    return result
+        sql = "SELECT * FROM aisles LIMIT 3;"
+        cur.execute(sql)
 
+        # get all column names
+        columns = [desc[0] for desc in cur.description]
+        # get all data
+        rows = cur.fetchall()
+
+        # build json
+        result = rows_to_json(columns, rows)
+        # print(result)
+
+        # end time
+        compute_time = time.time() - start_time
+        cur.close()
+        conn.close()
+        
+        
+
+        # response data
+        response_data = {
+            'data': result,
+            'columns': columns,
+            'compute_time': compute_time
+        }
+        return response(response_data)
+    except Exception as e:
+        return {'error': str(e)}
 
 
 @app.route('/runsqlquery', methods=['POST'])
 def run_mysql_query():
     # read post request data
     request_data = request.get_json()
-
-    print(request_data)
-     
-    # create a new connection
-    conn = pymysql.connect(host=config._DB_CONF['host'], 
-                           port=config._DB_CONF['port'], 
-                           user=config._DB_CONF['user'], 
-                           passwd=config._DB_CONF['passwd'], 
-                           db=config._DB_CONF['db'])
-    cur = conn.cursor()
-    # read the query to run from request 
     sql = request_data['sql']
-    # excure the query
-    cur.execute(sql)
-    
-    # get all column names
-    columns = [desc[0] for desc in cur.description]
+    # print(sql)
+    try:
+        # create mysql connection
+        conn = pymysql.connect(host=config._DB_CONF['host'],
+                               port=config._DB_CONF['port'],
+                               user=config._DB_CONF['user'],
+                               passwd=config._DB_CONF['passwd'],
+                               db=config._DB_CONF['db'])
+        cur = conn.cursor()
+        # caluclating the time from now
+        start_time = time.time()
 
-    # get all data
-    rows=cur.fetchall()
-    
-    # build json 
-    result = rows_to_json(columns,rows)
-    
-    # close connection
-    cur.close()
-    conn.close()
+        cur.execute(sql)
 
-    return result
-  	
+        # get all column names
+        columns = [desc[0] for desc in cur.description]
+        # get all data
+        rows = cur.fetchall()
+
+        # build json
+        result = rows_to_json(columns, rows)
+        # print(result)
+
+        # end time
+        compute_time = time.time() - start_time
+        cur.close()
+        conn.close()
+
+        # response data
+        response_data = {
+            'data': result,
+            'compute_time': compute_time
+        }
+        return response(response_data)
+    except Exception as e:
+        return {'error': str(e)}
+
+# todo wirte redshift query getter
+
+def run_redshift_query():
+    request_data = request.get_json()
+    sql = request_data['sql']
+    try:
+        # connect to redshift
+        conn = psycopg2.connect(host=config._REDSHIFT_CONF['host'],
+                                port=config._REDSHIFT_CONF['port'],
+                                user=config._REDSHIFT_CONF['user'],
+                                password=config._REDSHIFT_CONF['password'],
+                                database=config._REDSHIFT_CONF['database'])
+        cur = conn.cursor()
+        # caluclating the time from now
+        start_time = time.time()
+
+        cur.execute(sql)
+
+        # get all column names
+        columns = [desc[0] for desc in cur.description]
+        # get all data
+        rows = cur.fetchall()
+
+        # build json
+        result = rows_to_json(columns, rows)
+        # print(result)
+
+        # end time
+        compute_time = time.time() - start_time
+        cur.close()
+        conn.close()
+
+        # response data
+        response_data = {
+            'data': result,
+            'compute_time': compute_time
+        }
+        return response(response_data)
+    except Exception as e:
+        return {'error': str(e)}    
+
+
 @app.errorhandler(500)
 def server_error(e):
     logging.exception('An error occurred during a request.')
@@ -107,8 +190,8 @@ def server_error(e):
     See logs for full stacktrace.
     """.format(e), 500
 
+
 if __name__ == '__main__':
-    app.run(threaded=True,debug=True)
+    app.run(threaded=True, debug=True)
 
-## [END app]
-
+# [END app]
--- config.py
+++ config.py
@@ -1,12 +1,22 @@
 '''
   This is the config file which includes all ccnfiguation info here
 '''
-
+# RDS config
 _DB_CONF = {
- 'host':'database-2.chb5mphqelwh.us-east-1.rds.amazonaws.com',
+ 'host':'database-1.chb5mphqelwh.us-east-1.rds.amazonaws.com',
  'port':3306,
  'user':'admin',
- 'passwd':'example123',
+ 'passwd':'database',
  'db':'instacart'
 }
+
+# Redshift config
+_REDSHIFT_CONF = {
+  'host':'database-1.chb5mphqelwh.us-east-1.redshift.amazonaws.com',
+  'port':5439,
+  'user':'admin',
+  'password':'database',
+  'database':'instacart'  # this is the database name
+}
+
 
